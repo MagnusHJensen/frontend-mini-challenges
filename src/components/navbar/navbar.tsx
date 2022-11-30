b59b2a5817f709869c7fac664caca78a21e36a2d
MagnusHJensen <magnus.holm.jensen@lego.dk>
2022-11-30 07:33:44 +0100

    Udpdate navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,24 @@
-import { Box, HStack, Icon, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Hide,
+  HStack,
+  Icon,
+  IconButton,
+  Link,
+  Show,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Link as RouterLink, NavLink } from "react-router-dom";
-import { FaGithub } from "react-icons/fa";
+import { FaBars, FaGithub, FaHamburger } from "react-icons/fa";
 
 type NavigationElement = {
   text: string;
@@ -19,35 +37,75 @@ const navigationElements: NavigationElement[] = [
 ];
 
 export const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // TODO: Fix this mess of a navbar.
+
+  const navItems: JSX.Element = (
+    <Stack direction={{ md: "row", base: "column" }}>
+      {navigationElements.map((navElement) => {
+        return (
+          <Link to={navElement.link} key={navElement.text} as={RouterLink}>
+            {navElement.text}
+          </Link>
+        );
+      })}
+    </Stack>
+  );
+
   return (
-    <HStack
-      padding="1rem"
-      width="100%"
-      backgroundColor="gray.500"
-      justifyContent="space-between"
-    >
-      <HStack>
-        {navigationElements.map((navElement) => {
-          return (
-            <Link
-              to={navElement.link}
-              key={navElement.text}
-              color="white"
-              as={RouterLink}
-            >
-              {navElement.text}
-            </Link>
-          );
-        })}
-      </HStack>
-      <Box>
-        <Link
-          href="https://github.com/MagnusHJensen/frontend-mini-challenges"
-          target="_blank"
-        >
-          <Icon width="32px" height="32px" as={FaGithub} />
-        </Link>
-      </Box>
-    </HStack>
+    <Box padding="1rem" width="100%" backgroundColor="gray.500">
+      <Flex
+        justifyContent={{ base: "space-between", md: "flex-start" }}
+        width="100%"
+        alignItems="center"
+      >
+        <Box mr={8} width="fit-content">
+          <Text>Frontend Challenges</Text>
+        </Box>
+        <Show above="md">
+          <Flex
+            justifyContent="space-between"
+            width="80%"
+            alignItems="center"
+            textColor="white"
+          >
+            {navItems}
+
+            <Box>
+              <Link
+                href="https://github.com/MagnusHJensen/frontend-mini-challenges"
+                target="_blank"
+              >
+                <Icon width="32px" height="32px" as={FaGithub} />
+              </Link>
+            </Box>
+          </Flex>
+        </Show>
+        <Hide above="md">
+          <Drawer
+            placement="left"
+            isOpen={isOpen}
+            onClose={onClose}
+            closeOnEsc
+            closeOnOverlayClick
+          >
+            <DrawerOverlay />
+            <DrawerContent textColor="black">
+              <DrawerHeader>Frontend Challenges</DrawerHeader>
+              <DrawerBody>{navItems}</DrawerBody>
+            </DrawerContent>
+          </Drawer>
+          <IconButton
+            variant="ghost"
+            _hover={{ transform: "scale(1.2)", textColor: "white" }}
+            _active={{}}
+            icon={<FaBars />}
+            aria-label="Hamburger menu"
+            onClick={onOpen}
+          />
+        </Hide>
+      </Flex>
+    </Box>
   );
 };
